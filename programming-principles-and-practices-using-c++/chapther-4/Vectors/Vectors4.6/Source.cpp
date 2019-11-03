9a19d738eb21f38142f653b7beaadce8634849e2
star-lord <alekrifai97@gmail.com>
2019-11-03 17:36:20 +0200

    Chapter 4

---
+++ programming-principles-and-practices-using-c++/chapther-4/Vectors/Vectors4.6/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	vector<string> words;
+
+	for (string temp; cin >> temp; )
+	{
+		words.push_back(temp);
+	}
+
+	for (int i = 0; i < words.size(); i++)
+	{
+		if (i != 0 && words[i] == words[i - 1])
+		{
+			cout << "BLEEP" << " ";
+		}
+		else
+		{
+			cout << words[i] << " ";
+		}
+	}
+
+	return 0;
+}
